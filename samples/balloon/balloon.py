8c9677f5c544e0652be19f681b375ed7c228e470
AyaRabea <51236433+AyaRabea@users.noreply.github.com>
2021-09-22 14:25:22 +0200

    Update balloon.py

--- samples/balloon/balloon.py
+++ samples/balloon/balloon.py
@@ -33,9 +33,10 @@ import json
 import datetime
 import numpy as np
 import skimage.draw
+sys.path.append("/content/Mask_RCNN/mrcnn/")
 
 # Root directory of the project
-ROOT_DIR = os.path.abspath("../../")
+ROOT_DIR = os.path.abspath("../content/Mask_RCNN/")
 
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
